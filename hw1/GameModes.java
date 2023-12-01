86309680cd577f5117a122a0df63fd10dfc162f6
Vadim-Grender <116440694+Vadim-Grender@users.noreply.github.com>
2023-12-01 08:44:56 +0300

    Add files via upload

---
+++ hw1/GameModes.java
@@ -0,0 +1,8 @@
+public class GameModes {
+    static Map map;
+    public static final int HUMAN_VS_AI = 0;
+    public static final int HUMAN_VS_HUMAN = 1;
+    static void startNewGame(int mode, int fieldSizeX, int fieldSizeY, int winLength) {
+        map.startNewGame(mode, fieldSizeX, fieldSizeY, winLength);
+    }
+}
